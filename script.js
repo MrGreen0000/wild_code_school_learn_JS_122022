19305ba51c9faa7ed815888283b8c18464786b0e
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-01-25 08:00:19 +0100

    Les Fonctions JS

--- script.js
+++ script.js
@@ -1,80 +1,41 @@
-let today = new Date();
-document.body.innerHTML = "<h1>Today's date is " + today + "</h1>"
-document.body.style.backgroundColor = "pink";
-document.body.style.color = "white";
-
-let p = document.createElement("p");
-let t = document.createTextNode("Paragraph text.");
-p.appendChild(t);
-document.body.appendChild(p);
-
-console.log(1 !== 1);
-
-let myName = "Naji";
-console.log(myName);
-document.body.innerHTML = "<h1> My name is " + myName + "</h1>";
-
-let userAge = 44;
-let fruits = ['apple', 'banana', 'kiwi'];
-let userCar = {
-    model: "BMW",
-    year: "2000"
-};
-let sayMyName = function () {
-    console.log("My name is Bob !")
-};
-
-let myNames = "Bob";
-console.log(myNames);
-myNames = "Paul";
-console.log(myNames);
-typeof 1;
-
-let a = 1;
-a = 2;
-console.log(a);
-
-const name = "Paul";
-
-if (name === "Paul") {
-    console.log("Welcome, Paul");
-} else {
-    console.log("Go away!");
+function helloWorld (){
+    console.log("Hello, ");
+    console.log("World !");
 }
 
-const names = "Bob";
+helloWorld();
 
-if (names === "Bob") {
-    console.log("Hello, Bob!");
-} else {
-    console.log("You're not Bob, go away");
+function sayHello(name, age){
+    console.log("Hello " + name + "!" + " "+ age + " ans") ;
 }
 
-const userName = prompt("What's your name?");
-console.log(userName);
+sayHello("Naji", 44);
 
-const password = prompt("What's your password");
+const name = "Alexandra";
 
-if (password === "secret") {
-    console.log("Welcome !");
-} else {
-    console.log("Wrong password !");
+console.log(`Hello ${name}`);
+console.log(`C'est vrai: ${1 < 2}`);
+
+function sum(a,b){
+    return a + b;
 }
 
-const age = prompt("How old are you?");
-console.log(typeof(age))
+console.log(sum(1,2))
 
-const ageNumber = parseInt(age);
-console.log(typeof(ageNumber));
+function login(name, password){
+    if(name === "Bob" && password === "secret"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
-const user = prompt("How old are you ?");
+let userName = prompt("What's your name?");
+let userPassword = prompt("What's your password?");
 
-if (user < 3) {
-    console.log("Hello, Baby !");
-} else if (user < 18) {
-    console.log("Hi !");
-} else if (user < 100) {
-    console.log("Greetings");
-} else {
-    console.log("Wow...");
+if(login(userName, userPassword)){
+    console.log("Welcome!");
+}else{
+    console.log("Wrong credentials...");
 }
